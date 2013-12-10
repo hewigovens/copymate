74b7451be258cbeca3a47d2b7e8f35642351e9ae
hewigovens <hewigovens@gmail.com>
2013-12-11 00:05:42 +0800

    fix cert name

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -5,6 +5,6 @@ openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/developer_id.p12.e
 
 security create-keychain -p travis travis-build.keychain
 
-security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/travis-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/developer_id.cer -k ~/Library/Keychains/travis-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/developer_id.p12 -k ~/Library/Keychains/travis-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
